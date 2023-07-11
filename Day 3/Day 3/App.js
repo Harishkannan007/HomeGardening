7d81b9b0b73b4e1e308d1664ce0cb3197ab8cf08
Harishkannan007 <125952597+Harishkannan007@users.noreply.github.com>
2023-07-11 16:05:36 +0530

    Add files via upload

---
+++ Day 3/Day 3/App.js
@@ -0,0 +1,16 @@
+
+// import logo from './logo.svg';
+import './App.css';
+import Reg from './Reg';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+     <Reg/>
+     
+    </div>
+  );
+}
+
+export default App;
